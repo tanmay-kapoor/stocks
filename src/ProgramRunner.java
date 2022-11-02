c70858cdf616ef72daf1d45c5c72c4908b1f53f7
tanmaykapoor <tanmay.skater@gmail.com>
2022-11-02 00:26:41 -0400

    use local csv files for fetching stock data

--- src/ProgramRunner.java
+++ src/ProgramRunner.java
@@ -2,6 +2,7 @@ import controllers.Controller;
 import controllers.StockController;
 import models.api.AlphaVantageDemo;
 import models.api.ShareApi;
+import models.api.StockApi;
 import views.Menu;
 import views.StockMenu;
 
@@ -12,7 +13,8 @@ public class ProgramRunner {
   public static void main(String[] args) {
 
     Menu menu = new StockMenu(System.in, System.out);
-    ShareApi api = new AlphaVantageDemo();
+//    ShareApi api = new AlphaVantageDemo();
+    ShareApi api = new StockApi();
     String folder = "stocks";
 
     Controller controller = new StockController(menu, api, folder);
