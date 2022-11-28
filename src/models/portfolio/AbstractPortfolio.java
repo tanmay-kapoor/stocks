6c60a0839822dc5d0d8e639b0c9cd2201dc2bd2c
tanmaykapoor <tanmay.skater@gmail.com>
2022-11-28 16:14:16 -0500

    model imporvament for composition

--- src/models/portfolio/AbstractPortfolio.java
+++ src/models/portfolio/AbstractPortfolio.java
@@ -234,12 +234,14 @@ abstract class AbstractPortfolio implements Portfolio {
     for (String stock : stocks.keySet()) {
       Log log = stocks.get(stock);
       Set<Details> d = new TreeSet<>(Comparator.comparing(Details::getPurchaseDate));
+      Details latest = null;
       for (Details details : log.getDetailsSet()) {
         if (details.getPurchaseDate().compareTo(date) <= 0) {
-          d.add(details);
+          latest = details;
         }
       }
-      if (d.size() > 0) {
+      if(latest != null) {
+        d.add(latest);
         Log logCopy = new Log(d, log.getLastSoldDate());
         filteredStocks.put(stock, logCopy);
       }
